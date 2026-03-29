412e29b096f91131aaf87d39af5d383d2cc17766
allan007ca <allaniaccollins@gmail.com>
2026-03-29 16:23:45 -0700

    Hour 8 Done

--- amcnulty_Hour8_P1/Assets/SideQuest.cs
+++ amcnulty_Hour8_P1/Assets/SideQuest.cs
@@ -1,3 +1,6 @@
 /*
-The method's name is TakeDamage, the variable type that the method returns is int, there is one parameter which is int damageAmount, the code in the method's block states the health, calculates it, and returns it'
+The method's name is TakeDamage
+The variable type that the method returns is int
+There is one parameter which is int damageAmount
+The code in the method's block states the health, calculates it, and returns it(int health = 100; return health - damageAmount;)
 */
