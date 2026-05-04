a39d27979fbea3263c7d4d6235cbb25d6c0a40b2
allan007ca <allaniaccollins@gmail.com>
2026-05-03 21:07:31 -0700

    Hour 11 Lined Up Lamps

--- amcnulty_Hour11/Assets/Scripts/PrefabGenerator.cs
+++ amcnulty_Hour11/Assets/Scripts/PrefabGenerator.cs
@@ -6,16 +6,27 @@ public class PrefabGenerator : MonoBehaviour
 {
     public GameObject prefab;
 
+    void Start()
+    {
+        
+        for (int i = 0; i < 10; i++)
+        {
+            Vector3 spawnPosition = new Vector3(i * 2f, 0, 0);  
+            Instantiate(prefab, spawnPosition, Quaternion.identity);
+        }
+    }
+
     void Update()
     {
         // Whenever we hit the B key we will generate a prefab at the
         // position of the original prefab
-        // Whenever we hit the space key, we will generate a prefab at the
-        // position of the spawn object that this script is attached to
         if (Input.GetKeyDown(KeyCode.B))
         {
             Instantiate(prefab);
         }
+        
+        // Whenever we hit the space key, we will generate a prefab at the
+        // position of the spawn object that this script is attached to
         if (Input.GetKeyDown(KeyCode.Space))
         {
             Instantiate(prefab, transform.position, transform.rotation);
