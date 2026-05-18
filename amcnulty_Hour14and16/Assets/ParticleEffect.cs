bca87eb7215d2fd0eeb55259b9399f7ff818d2d4
allan007ca <allaniaccollins@gmail.com>
2026-05-17 20:51:02 -0700

    Hour 16

---
+++ amcnulty_Hour14and16/Assets/ParticleEffect.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleEffect : MonoBehaviour
+/*
+Changed rotation and size
+Changed color to resemble a blue ember
+changed its gravity to float upwards from a downward position
+Extended the lifetime of the particles
+Sped up the start speed
+Added start size curve
+*/
