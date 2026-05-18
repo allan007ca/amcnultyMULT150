6934a97d192c4fb73a6770f8aed61d03e9ed1227
allan007ca <allaniaccollins@gmail.com>
2026-05-17 19:37:50 -0700

    Hour 14

---
+++ amcnulty_Hour14and16/Assets/UserInterface.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UserInterface : MonoBehaviour
+/*
+I changed the background image to a stock cardboard texture
+I added a title with a bold font, a new font, and made it italicized
+Changed colors for text
+Added a body of text to describe the game's core idea.
+Gave the button different colors in its reactivity and changed its text
+Resized button
+
+*/
