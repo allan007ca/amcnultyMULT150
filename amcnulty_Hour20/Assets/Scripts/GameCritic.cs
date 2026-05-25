36e079dcfa4962cef3cb7b9bef8bc910e1ee030d
allan007ca <allaniaccollins@gmail.com>
2026-05-24 22:31:55 -0700

    Hour 20

---
+++ amcnulty_Hour20/Assets/Scripts/GameCritic.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameCritic : MonoBehaviour
+{
+/* 
+Is the game too easy or hard?
+
+The game was not too hard, but it felt far too short. It was a good difficulty for me, just wish it kept on going.
+
+What changes would you do to make it easier or harder?
+
+I would make the obstacles give less slowness, but make it stackable.
+
+What would make the game more appealing?
+
+The game would be great if it kept on going until the player was too slow to keep up, or lose after a certain amount of obstacle collisions.
+
+What parts of the game are fun? 
+
+I like the asthetic, it reminds me of Minion Rush, or the mobile Iron Man 3 game. 
+
+What parts of the game are tedious?
+
+Sometimes the movements felt too slow before colliding with an obstacle
+
+Changes made:
+
+The obstacle prefeb was changing into a lit bomb. It had rope and balck ceramic textures. The fire was simulated with light and particles. 
+
+The Powerup prefab was changed to money with a radiant glow. I felt momey would be more of an enticement than yellow balls. Furthermore, I orientated the money to look as though it it flying by. I made the particle system shoot out money to make it seem as though it is a thinning stack of cash. 
+
+*/
+}
