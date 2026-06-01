918f0f28aae7ae5f947c3ff46a56d1f75e2ef8d3
allan007ca <allaniaccollins@gmail.com>
2026-05-31 18:17:44 -0700

    Update

--- amcnulty_Final/Assets/Scripts/Modifications.cs
+++ amcnulty_Final/Assets/Scripts/Modifications.cs
@@ -9,34 +9,55 @@ GAUNTLET RUNNER
 ================================================================
 1 - directional light (colour, intensity, etc)
 modifications:
+I changed the color to hot pink to fit the rest of the level's theme and increased its intensity to 3.09
+
 2 - fog
 modifications:
+Added a dark grey fog to make it harder for the player to anticipate incoming powerups and threats.
+
 3 - create & apply skybox and customise it (atmosphere thickness, sky tint,
 exposure, etc)
 modifications:
+Made the sky tint grey and the ground pink with medium exposure to create a misterious orange skybox.
+
 OBSTACLE PREFAB
 4 - add or modify light component (colour, range, intensity, etc)
 modifications:
+I made the particle effect easier to see by increasing size and I increased the intensity of the fire's light. I also changed it from a dark red to a orange to more realistically capture fire.
+
 POWERUP PREFAB
 5 - replace with a new complex object comprised of 3 or more shapes
 (different from previously made object)
 modifications:
+Changed it from a sigular dollar bill to a stack of bills. I altered the particle system's size to be more in line with the different model.
+
 6 - use gameobject hierarchy to keep the powerup prefab organised
 [no record necessary]
+
 7 - apply at least 2 new textures
 (add source url or package name)
-texture source 1:
-texture source 2:
+texture source 1:https://t4.ftcdn.net/jpg/02/35/66/97/360_F_235669706_pb7eAtgc99Qpevp5kCOIJ25m7oGVPQ05.jpg
+texture source 2:https://www.shutterstock.com/image-vector/currency-strap-make-easy-organize-260nw-264401207.jpg
+
 8 - add particle system component and customise at least 3 parameters
-parameter 1:
-parameter 2:
-parameter 3:
+parameter 1:3D Start Size
+parameter 2:Simulation Speed
+parameter 3:Start Lifetime
+
 WALL
 9 - convert a wall to prefab, create a variant, and use the variant to
 replace the other wall in the scene
 [no record necessary]
+https://i.pinimg.com/736x/66/17/41/6617412c491793f555221ce53c7336ff.jpg
+https://t4.ftcdn.net/jpg/03/38/88/03/360_F_338880391_I5j28kNo11FLupFU6vj3sgQlCOkKNwLW.jpg
+
 10 - modify wall prefab material tint so that the 2 walls are different in
 appearance or colour
 [no record necessary]
+https://filterforge.com/filters/10587.jpg
+
+Audio:
+https://www.myinstants.com/en/instant/homer-drops-his-donut-29546/
+https://www.youtube.com/watch?v=v1lqtZE4n18
 */
 }
